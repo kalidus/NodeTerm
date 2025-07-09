cb4eb38b065c9ef9468cf1124f6e4bce7397e9d4
kalidus <kalidus@example.com>
2025-07-09 11:00:08 +0200

    añadido 12 temas nuevos a cada uno de los 3 archivos de temas:

--- src/themes/status-bar-themes.js
+++ src/themes/status-bar-themes.js
@@ -178,5 +178,185 @@ export const statusBarThemes = {
       networkDownColor: '#21c7a8',
       sparklineColor: '#82aaff'
     }
+  },
+  'Tokyo Night': {
+    name: 'Tokyo Night',
+    colors: {
+      background: '#1a1b26',
+      text: '#c0caf5',
+      border: '#414868',
+      iconColor: '#7aa2f7',
+      cpuBarColor: '#7aa2f7',
+      memoryBarColor: '#9ece6a',
+      diskBarColor: '#e0af68',
+      networkUpColor: '#9ece6a',
+      networkDownColor: '#7dcfff',
+      sparklineColor: '#7aa2f7'
+    }
+  },
+  'Catppuccin': {
+    name: 'Catppuccin',
+    colors: {
+      background: '#1e1e2e',
+      text: '#cdd6f4',
+      border: '#45475a',
+      iconColor: '#89b4fa',
+      cpuBarColor: '#89b4fa',
+      memoryBarColor: '#a6e3a1',
+      diskBarColor: '#f9e2af',
+      networkUpColor: '#a6e3a1',
+      networkDownColor: '#94e2d5',
+      sparklineColor: '#89b4fa'
+    }
+  },
+  'Synthwave 84': {
+    name: 'Synthwave 84',
+    colors: {
+      background: '#2a2139',
+      text: '#f92aad',
+      border: '#6b5b95',
+      iconColor: '#03edf9',
+      cpuBarColor: '#03edf9',
+      memoryBarColor: '#72f1b8',
+      diskBarColor: '#f97e72',
+      networkUpColor: '#72f1b8',
+      networkDownColor: '#03edf9',
+      sparklineColor: '#03edf9'
+    }
+  },
+  'Palenight': {
+    name: 'Palenight',
+    colors: {
+      background: '#292d3e',
+      text: '#a6accd',
+      border: '#676e95',
+      iconColor: '#82aaff',
+      cpuBarColor: '#82aaff',
+      memoryBarColor: '#c3e88d',
+      diskBarColor: '#ffcb6b',
+      networkUpColor: '#c3e88d',
+      networkDownColor: '#89ddff',
+      sparklineColor: '#82aaff'
+    }
+  },
+  'High Contrast': {
+    name: 'High Contrast',
+    colors: {
+      background: '#000000',
+      text: '#ffffff',
+      border: '#808080',
+      iconColor: '#00ffff',
+      cpuBarColor: '#00ffff',
+      memoryBarColor: '#00ff00',
+      diskBarColor: '#ffff00',
+      networkUpColor: '#00ff00',
+      networkDownColor: '#0000ff',
+      sparklineColor: '#00ffff'
+    }
+  },
+  'Ayu Dark': {
+    name: 'Ayu Dark',
+    colors: {
+      background: '#0f1419',
+      text: '#e6e1cf',
+      border: '#323232',
+      iconColor: '#59c2ff',
+      cpuBarColor: '#59c2ff',
+      memoryBarColor: '#b8cc52',
+      diskBarColor: '#ff8f40',
+      networkUpColor: '#b8cc52',
+      networkDownColor: '#95e6cb',
+      sparklineColor: '#59c2ff'
+    }
+  },
+  'Zenburn': {
+    name: 'Zenburn',
+    colors: {
+      background: '#3f3f3f',
+      text: '#dcdccc',
+      border: '#709080',
+      iconColor: '#94bff3',
+      cpuBarColor: '#94bff3',
+      memoryBarColor: '#c3bf9f',
+      diskBarColor: '#f0dfaf',
+      networkUpColor: '#c3bf9f',
+      networkDownColor: '#93e0e3',
+      sparklineColor: '#94bff3'
+    }
+  },
+  'Tomorrow Night': {
+    name: 'Tomorrow Night',
+    colors: {
+      background: '#1d1f21',
+      text: '#c5c8c6',
+      border: '#969896',
+      iconColor: '#81a2be',
+      cpuBarColor: '#81a2be',
+      memoryBarColor: '#b5bd68',
+      diskBarColor: '#f0c674',
+      networkUpColor: '#b5bd68',
+      networkDownColor: '#8abeb7',
+      sparklineColor: '#81a2be'
+    }
+  },
+  'Oceanic Next': {
+    name: 'Oceanic Next',
+    colors: {
+      background: '#1b2b34',
+      text: '#d8dee9',
+      border: '#65737e',
+      iconColor: '#6699cc',
+      cpuBarColor: '#6699cc',
+      memoryBarColor: '#99c794',
+      diskBarColor: '#fac863',
+      networkUpColor: '#99c794',
+      networkDownColor: '#5fb3b3',
+      sparklineColor: '#6699cc'
+    }
+  },
+  'Atom One Light': {
+    name: 'Atom One Light',
+    colors: {
+      background: '#fafafa',
+      text: '#383a42',
+      border: '#4f525d',
+      iconColor: '#4078f2',
+      cpuBarColor: '#4078f2',
+      memoryBarColor: '#50a14f',
+      diskBarColor: '#c18401',
+      networkUpColor: '#50a14f',
+      networkDownColor: '#0184bc',
+      sparklineColor: '#4078f2'
+    }
+  },
+  'Breeze': {
+    name: 'Breeze',
+    colors: {
+      background: '#232629',
+      text: '#fcfcfc',
+      border: '#7f8c8d',
+      iconColor: '#3daee9',
+      cpuBarColor: '#3daee9',
+      memoryBarColor: '#1cdc9a',
+      diskBarColor: '#fdbc4b',
+      networkUpColor: '#1cdc9a',
+      networkDownColor: '#16a085',
+      sparklineColor: '#3daee9'
+    }
+  },
+  'Spacemacs': {
+    name: 'Spacemacs',
+    colors: {
+      background: '#292b2e',
+      text: '#b2b2b2',
+      border: '#5d4d7a',
+      iconColor: '#4f97d7',
+      cpuBarColor: '#4f97d7',
+      memoryBarColor: '#67b11d',
+      diskBarColor: '#b1951d',
+      networkUpColor: '#67b11d',
+      networkDownColor: '#2d9574',
+      sparklineColor: '#4f97d7'
+    }
   }
 };
