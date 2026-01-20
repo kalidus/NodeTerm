128d4ab56075c6ae5de0d81c7e599729371251ae
kalidus <kalidus@example.com>
2026-01-20 14:36:01 +0100

    primera version version final hometab! a ver q sale. v1

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -7,7 +7,8 @@ export const STORAGE_KEYS = {
   SIDEBAR_START_COLLAPSED: 'sidebar_start_collapsed',
   DEFAULT_LOCAL_TERMINAL: 'nodeterm_default_local_terminal',
   HOME_TAB_LOCAL_TERMINAL_VISIBLE: 'homeTab_localTerminalVisible',
-  HOME_TAB_STATUS_BAR_VISIBLE: 'homeTab_statusBarVisible'
+  HOME_TAB_STATUS_BAR_VISIBLE: 'homeTab_statusBarVisible',
+  HOME_TAB_RIGHT_COLUMN_COLLAPSED: 'homeTab_rightColumnCollapsed'
 };
 
 // Constantes de grupos
